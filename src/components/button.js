1952f5ae72cb7cf8fe41ab8ff356ef8b1a2a3216
Alex Tait <alexandra.tait@rangle.io>
2018-11-28 10:00:49 -0500

    adding all categories

--- src/components/button.js
+++ src/components/button.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 export const Button = ({ onClick, children }) => {
   return <button onClick={onClick}>{children}</button>;
 };
+//TODO: update button to be a styled component expressing the theme
 
 // export const Button = styled.button`
 //   color: ${props => props.theme.btnPrimary}
